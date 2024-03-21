ee5f6c04fbf9fe33a08afb3b492876feb22bf22a
Ammar Elkosi <ammarrelkosii@yahoo.com>
2024-03-21 23:24:01 +0200

    data migration to static

--- src/components/projectDetailsHeroSection/ProjectDetailsHeroSection.tsx
+++ src/components/projectDetailsHeroSection/ProjectDetailsHeroSection.tsx
@@ -9,15 +9,14 @@ const ProjectDetailsHeroSection = ({ data }: any) => {
     : null;
   const projectVideos = data.project_videos.data
     ? data.project_videos.data.map((each: any) => (
-        <video
-          src={`${each.attributes.url}`}
-          autoPlay
+        <iframe
           width={"100%"}
-          height={"100%"}
-          controls
-          preload="metadata"
-          muted
-        />
+          height={600}
+          src={`${each.attributes.url}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       ))
     : null;
 
